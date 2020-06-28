35b7eb8a45607eb8206ae819005c8edc00f3d2cd
Shyaboi <ian.shane.sears@gmail.com>
2020-06-27 22:52:19 -0700

    Updates

---
+++ precache-manifest.13ee0bfc896ddfcd44705a8c28d16aeb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8793c1315b5a344948a7a290e8728cc",
+    "url": "/shyaboi/roosterreact/index.html"
+  },
+  {
+    "revision": "7bc7afc7563078800c56",
+    "url": "/shyaboi/roosterreact/static/css/2.fa4df4b2.chunk.css"
+  },
+  {
+    "revision": "e5406848edab98c721e1",
+    "url": "/shyaboi/roosterreact/static/css/main.55477bff.chunk.css"
+  },
+  {
+    "revision": "7bc7afc7563078800c56",
+    "url": "/shyaboi/roosterreact/static/js/2.fde8e441.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/shyaboi/roosterreact/static/js/2.fde8e441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5406848edab98c721e1",
+    "url": "/shyaboi/roosterreact/static/js/main.68525cf2.chunk.js"
+  },
+  {
+    "revision": "ef13f16253fc19456507",
+    "url": "/shyaboi/roosterreact/static/js/runtime-main.35b1cd3e.js"
+  },
+  {
+    "revision": "d679e95e3e1a4102ea2a1ef27bd34899",
+    "url": "/shyaboi/roosterreact/static/media/banner-1.d679e95e.jpg"
+  },
+  {
+    "revision": "541a7d9ecf6e06e41906b9424af075ac",
+    "url": "/shyaboi/roosterreact/static/media/banner-2.541a7d9e.jpg"
+  },
+  {
+    "revision": "7bc311e4e86ac578b19c3e27c1d2aa01",
+    "url": "/shyaboi/roosterreact/static/media/facebook.7bc311e4.svg"
+  },
+  {
+    "revision": "83af6455cc310dffacba4d9981178c62",
+    "url": "/shyaboi/roosterreact/static/media/hero.83af6455.jpg"
+  },
+  {
+    "revision": "4631d634d77566ea790be3171fd1adea",
+    "url": "/shyaboi/roosterreact/static/media/home-expertise.4631d634.svg"
+  },
+  {
+    "revision": "2647fa6b7f23e3a31808e475df20b241",
+    "url": "/shyaboi/roosterreact/static/media/home-hygiene.2647fa6b.svg"
+  },
+  {
+    "revision": "c97ae485885baadfc689ca82304bcfc7",
+    "url": "/shyaboi/roosterreact/static/media/home-lab.c97ae485.svg"
+  },
+  {
+    "revision": "79cb05a3c9e1bf2f1c335e1acfa02296",
+    "url": "/shyaboi/roosterreact/static/media/home-retention.79cb05a3.svg"
+  },
+  {
+    "revision": "cefc803512d923ce6d6d3f6f6cd7feb9",
+    "url": "/shyaboi/roosterreact/static/media/img-1.cefc8035.jpg"
+  },
+  {
+    "revision": "417149bccf5f7ef142c1e2aa48778220",
+    "url": "/shyaboi/roosterreact/static/media/instagram.417149bc.svg"
+  }
+]);
